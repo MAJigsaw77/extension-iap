4eb855f873cf5a9a89cc3ecec3dedba0f65b860a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-03 18:38:59 +0200

    Update ProductDetails.hx

--- iap/ProductDetails.hx
+++ iap/ProductDetails.hx
@@ -1,49 +1,36 @@
-package extension.iap;
+package iap;
 
-class ProductDetails
+class IAPProductDetails
 {
-	
-	public var productID(default, null): String;
-    public var localizedTitle(default, null): String;
-    public var localizedDescription(default, null): String;
-    public var price(default, null): Float;
-    public var localizedPrice(default, null): String;
-	public var priceCurrencyCode(default,null):String;
-	public var priceAmountMicros(default,null):Float;
-	public var type(default, null): String;		//(android)
-	
-	public function new(dynObj:Dynamic) 
+	public var productID(default, null):String;
+	public var type(default, null):String;
+	public var localizedPrice(default, null):String;
+	public var localizedTitle(default, null):String;
+	public var localizedDescription(default, null):String;
+	public var priceAmountMicros(default, null):Float;
+	public var priceCurrencyCode(default, null):String;
+	public var price(default, null):Float;
+
+	public function new(dynObj:Dynamic)
 	{
-		// Handle both Android and iOS Ids
-		productID = Reflect.hasField(dynObj, "productId")? Reflect.field(dynObj, "productId") : Reflect.field(dynObj, "productID");
-		type = cast Reflect.field(dynObj, "type");
+		productID = Reflect.hasField(dynObj, 'productId') ? Reflect.field(dynObj, 'productId') : Reflect.field(dynObj, 'productID');
+
+		type = cast Reflect.field(dynObj, 'type');
+
 		#if ios
-			localizedPrice = cast Reflect.field(dynObj, "localizedPrice");
-			priceAmountMicros = cast Reflect.field(dynObj, "priceAmountMicros");
-			priceCurrencyCode = cast Reflect.field(dynObj, "priceCurrencyCode");
-			localizedDescription = cast Reflect.field(dynObj, "localizedDescription");
-			localizedTitle = cast Reflect.field(dynObj, "localizedTitle");
+		localizedPrice = cast Reflect.field(dynObj, 'localizedPrice');
+		localizedDescription = cast Reflect.field(dynObj, 'localizedDescription');
+		localizedTitle = cast Reflect.field(dynObj, 'localizedTitle');
+		priceAmountMicros = cast Reflect.field(dynObj, 'priceAmountMicros');
+		priceCurrencyCode = cast Reflect.field(dynObj, 'priceCurrencyCode');
 		#else
-			localizedPrice = cast Reflect.field(dynObj, "price");
-			priceAmountMicros = cast Reflect.field(dynObj, "price_amount_micros");
-			priceCurrencyCode = cast Reflect.field(dynObj, "price_currency_code");
-			localizedDescription = cast Reflect.field(dynObj, "description");
-			localizedTitle = cast Reflect.field(dynObj, "title");
+		localizedPrice = cast Reflect.field(dynObj, 'price');
+		localizedDescription = cast Reflect.field(dynObj, 'description');
+		localizedTitle = cast Reflect.field(dynObj, 'title');
+		priceAmountMicros = cast Reflect.field(dynObj, 'price_amount_micros');
+		priceCurrencyCode = cast Reflect.field(dynObj, 'price_currency_code');
 		#end
-		price = priceAmountMicros / 1000 / 1000;
+
+		price = priceAmountMicros / 1e6;
 	}
-	
-	public function toString() :String {
-		var res:String = "ProductDetails: { ";
-		if (Reflect.fields(this).length > 0) {
-			for (fieldLabel in Reflect.fields(this)) {
-				res += fieldLabel + ": " + Reflect.field(this, fieldLabel) + ", ";
-			}
-			res = res.substring(0, res.length - 1);
-		}
-		
-		res += " }";
-		return res;
-	}
-	
 }
