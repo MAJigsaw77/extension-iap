e800b1c1829c463853dd6c0af6629f19ba8ecd52
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-03 18:08:24 +0200

    Update IAP.hx

--- iap/android/IAP.hx
+++ iap/android/IAP.hx
@@ -13,45 +13,6 @@ import lime.system.JNI;
 import openfl.utils.JNI;
 #end
 
-/**
- * Provides convenience methods and properties for in-app purchases (Android & iOS).
- * The methods and properties are static, so there's no need to instantiate an instance,
- * but an initialization is required prior to the first use.
- * Every method is asynchronous (non-blocking). The callbacks always fire events indicating
- * the success or failure of every operation.
- *
- * The first step is to initialize the extension. You do so by calling the {@link #initialize}
- * method. The result comes with a PURCHASE_INIT or PURCHASE_INIT_FAILED IAPEvent. Also, the
- * available property will tell if you can use the extension at any time.
- *
- * Although we aim to provide a unified API for every target, there are some differences that
- * required to leave platform exclusive methods and properties. So you'll find different workflows.
- *
- * Android workflow:
- * ----------------
- *
- * After initialization is complete, you will typically want to request an inventory of owned
- * items and subscriptions. See {@link #queryInventory} and related events. This method can also be
- * used to retrieve a detailed list of products.
- *
- * Then you may want to buy items with the {@link #purchase} method, and if the item is consumable,
- * the {@link #consume} method should be called after a successful purchase.
- *
- * iOS workflow:
- * ------------
- *
- * After initialization is complete, you will typically want request details about the products
- * being sold {@link #requestProductData}, and also probably try to restore non consumable
- * items previously purchased by the user using the {@link #restore} method.
- *
- * Then you may want to buy items with the {@link #purchase} method. You don't need to call the
- * consume method for iOS, but for consumable items you may want to locally erase the purchase from
- * the Inventory.
- *
- * You may want to check the IAPEvent, Purchase and ProductDetails classes to explore further.
- *
- */
-
 @:allow(extension.iap) class IAP {
 
 	public static var initCounter:Int = 0;
