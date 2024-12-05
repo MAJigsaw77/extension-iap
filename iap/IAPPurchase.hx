8d5c9237f23b296c770cd7ccf1b709813e88d9fb
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-05 08:02:01 +0200

    Update IAPPurchase.hx

--- iap/IAPPurchase.hx
+++ iap/IAPPurchase.hx
@@ -5,30 +5,27 @@ import haxe.Int64;
 @:nullSafety
 class IAPPurchase
 {
-	public var key:String = '';
-	public var orderId:String = '';
-	public var packageName:String = '';
-	public var developerPayload:String = '';
-	public var productId:String = '';
-	public var purchaseTime:Int64 = 0;
-	public var token:String = '';
-	public var autoRenewing:Bool = false;
-	public var signature:String = '';
+	public final key:String;
+	public final orderId:String;
+	public final packageName:String;
+	public final developerPayload:String;
+	public final productId:String;
+	public final purchaseTime:Int64;
+	public final token:String;
+	public final autoRenewing:Bool;
+	public final signature:String;
 
-	public function new():Void {}
-
-	public static function fromJson(json:Dynamic):IAPPurchase
+	public function new(key:String, signature:String, json:Dynamic):Void
 	{
-		final purchase:IAPPurchase = new IAPPurchase();
-		purchase.key = json.key;
-		purchase.orderId = json.originalJson.orderId;
-		purchase.packageName = json.originalJson.packageName;
-		purchase.developerPayload = json.originalJson.developerPayload;
-		purchase.productId = json.originalJson.productId;
-		purchase.purchaseTime = json.originalJson.purchaseTime;
-		purchase.token = json.originalJson.purchaseToken;
-		purchase.autoRenewing = json.originalJson.autoRenewing;
-		purchase.signature = json.signature;
-		return purchase;
+		this.key = key;
+		this.signature = signature;
+
+		orderId = json.originalJson.orderId;
+		packageName = json.originalJson.packageName;
+		developerPayload = json.originalJson.developerPayload;
+		productId = json.originalJson.productId;
+		purchaseTime = json.originalJson.purchaseTime;
+		token = json.originalJson.purchaseToken;
+		autoRenewing = json.originalJson.autoRenewing;
 	}
 }
