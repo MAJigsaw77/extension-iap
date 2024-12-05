6d521907ef36f84f6f07a3e2427ded36383fbad5
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-05 08:04:56 +0200

    Update IAPProductDetails.hx

--- iap/IAPProductDetails.hx
+++ iap/IAPProductDetails.hx
@@ -5,36 +5,33 @@ import iap.IAPSubscriptionOffer;
 @:nullSafety
 class IAPProductDetails
 {
-	public var productID:String = '';
-	public var productType:String = '';
-	public var title:String = '';
-	public var name:String = '';
-	public var description:String = '';
-	public var formattedPrice:Null<String> = null;
-	public var priceAmountMicros:Float = 0.0;
-	public var priceCurrencyCode:Null<String> = null;
-	public var subscriptionOffers:Array<IAPSubscriptionOffer> = [];
+	public final productID:String;
+	public final productType:String;
+	public final title:String;
+	public final name:String;
+	public final description:String;
+	public final formattedPrice:String;
+	public final priceAmountMicros:Float;
+	public final priceCurrencyCode:String;
+	public final subscriptionOffers:Array<IAPSubscriptionOffer>;
 
-	public function new():Void {}
-
-	public static function fromJson(json:Dynamic):IAPProductDetails
+	public function new(json:Dynamic):Void
 	{
-		final productDetails:IAPProductDetails = new IAPProductDetails();
-		productDetails.productID = json.productID;
-		productDetails.productType = json.productType;
-		productDetails.title = json.title;
-		productDetails.name = json.name;
-		productDetails.description = json.description;
-		productDetails.formattedPrice = json.formattedPrice;
-		productDetails.priceAmountMicros = json.priceAmountMicros;
-		productDetails.priceCurrencyCode = json.priceCurrencyCode;
+		productID = json.productID;
+		productType = json.productType;
+		title = json.title;
+		name = json.name;
+		description = json.description;
+		formattedPrice = json.formattedPrice;
+		priceAmountMicros = json.priceAmountMicros;
+		priceCurrencyCode = json.priceCurrencyCode;
 
 		if (json.subscriptionOffers != null)
 		{
 			for (offerJson in json.subscriptionOffers)
-				productDetails.subscriptionOffers.push(IAPSubscriptionOffer.fromJson(offerJson));
+				subscriptionOffers.push(IAPSubscriptionOffer.fromJson(offerJson));
 		}
-
-		return productDetails;
+		else
+			subscriptionOffers = [];
 	}
 }
