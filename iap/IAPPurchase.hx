f3e1c8d13e2135737af35bd2b8a88dab167fb187
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-05 08:33:22 +0200

    Update IAPPurchase.hx

--- iap/IAPPurchase.hx
+++ iap/IAPPurchase.hx
@@ -5,21 +5,17 @@ import haxe.Int64;
 @:nullSafety
 class IAPPurchase
 {
-	public final key:String;
 	public final orderId:String;
 	public final packageName:String;
 	public final developerPayload:String;
-	public final productId:String;
+	public final productId:Array<String>;
 	public final purchaseTime:Int64;
 	public final token:String;
 	public final autoRenewing:Bool;
 	public final signature:String;
 
-	public function new(key:String, signature:String, json:Dynamic):Void
+	public function new(json:Dynamic, signature:String):Void
 	{
-		this.key = key;
-		this.signature = signature;
-
 		orderId = json.orderId;
 		packageName = json.packageName;
 		developerPayload = json.developerPayload;
@@ -27,5 +23,7 @@ class IAPPurchase
 		purchaseTime = json.purchaseTime;
 		token = json.purchaseToken;
 		autoRenewing = json.autoRenewing;
+
+		this.signature = signature;
 	}
 }
