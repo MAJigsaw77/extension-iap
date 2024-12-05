c10dee9bb7864eca484a19a20fda1e88b92ae33b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-05 19:02:27 +0200

    Update IAPPurchase.hx

--- iap/IAPPurchase.hx
+++ iap/IAPPurchase.hx
@@ -12,9 +12,8 @@ class IAPPurchase
 	public final purchaseTime:Int64;
 	public final token:String;
 	public final autoRenewing:Bool;
-
-	public final signature:String;
 	public final stringifyedJson:String;
+	public final signature:String;
 
 	public function new(json:Dynamic, signature:String):Void
 	{
@@ -26,7 +25,7 @@ class IAPPurchase
 		token = json.purchaseToken;
 		autoRenewing = json.autoRenewing;
 
-		this.signature = signature;
 		this.stringifyedJson = haxe.Json.stringify(json);
+		this.signature = signature;
 	}
 }
