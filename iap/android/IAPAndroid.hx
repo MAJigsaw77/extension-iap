3bf3eb345ede8ebbecf47a0f4c4faa16085431e6
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-06 09:53:42 +0200

    Update IAPAndroid.hx

--- iap/android/IAPAndroid.hx
+++ iap/android/IAPAndroid.hx
@@ -7,23 +7,89 @@ import iap.IAPPurchase;
 import lime.app.Event;
 import lime.utils.Log;
 
+/**
+ * A class that handles in-app purchase (IAP) functionality on Android, using JNI to interact with the native Android platform.
+ * Provides methods for initializing the IAP system, purchasing, consuming, acknowledging purchases, and querying product details.
+ */
 class IAPAndroid
 {
+	/**
+	 * Event dispatched when the IAP system initialization starts.
+	 * @param status A boolean indicating the initialization status.
+	 */
 	public static var onStarted(default, null):Event<Bool->Void> = new Event<Bool->Void>();
+
+	/**
+	 * Event dispatched when a purchase is successfully consumed.
+	 * @param purchase The IAPPurchase object representing the consumed purchase.
+	 */
 	public static var onConsume(default, null):Event<IAPPurchase->Void> = new Event<IAPPurchase->Void>();
+
+	/**
+	 * Event dispatched when consuming a purchase fails.
+	 * @param error A string representing the error message.
+	 * @param purchase The IAPPurchase object representing the purchase that failed to consume.
+	 */
 	public static var onFailedConsume(default, null):Event<String->IAPPurchase->Void> = new Event<String->IAPPurchase->Void>();
+
+	/**
+	 * Event dispatched when a purchase is successfully acknowledged.
+	 * @param purchase The IAPPurchase object representing the acknowledged purchase.
+	 */
 	public static var onAcknowledgePurchase(default, null):Event<IAPPurchase->Void> = new Event<IAPPurchase->Void>();
+
+	/**
+	 * Event dispatched when acknowledging a purchase fails.
+	 * @param error A string representing the error message.
+	 * @param purchase The IAPPurchase object representing the purchase that failed to acknowledge.
+	 */
 	public static var onFailedAcknowledgePurchase(default, null):Event<String->IAPPurchase->Void> = new Event<String->IAPPurchase->Void>();
+
+	/**
+	 * Event dispatched when a purchase is completed.
+	 * @param purchase The IAPPurchase object representing the completed purchase.
+	 */
 	public static var onPurchase(default, null):Event<IAPPurchase->Void> = new Event<IAPPurchase->Void>();
+
+	/**
+	 * Event dispatched when a purchase is canceled.
+	 * @param purchase The IAPPurchase object representing the canceled purchase.
+	 */
 	public static var onCanceledPurchase(default, null):Event<IAPPurchase->Void> = new Event<IAPPurchase->Void>();
+
+	/**
+	 * Event dispatched when a purchase fails.
+	 * @param error A string representing the error message.
+	 * @param purchase The IAPPurchase object representing the failed purchase.
+	 */
 	public static var onFailedPurchase(default, null):Event<String->IAPPurchase->Void> = new Event<String->IAPPurchase->Void>();
-	public static var onQueryProductDetailsFinished(default, null):Event<String->Void> = new Event<String->Void>();
+
+	/**
+	 * Event dispatched when product details are finished being queried.
+	 * @param result An array of IAPProductDetails objects representing the queried product details.
+	 */
+	public static var onQueryProductDetailsFinished(default, null):Event<Array<IAPProductDetails>->Void> = new Event<Array<IAPProductDetails>->Void>();
+
+	/**
+	 * Event dispatched when purchase details are finished being queried.
+	 * @param purchases An array of IAPPurchase objects representing the queried purchases.
+	 */
 	public static var onQueryPurchasesFinished(default, null):Event<Array<IAPPurchase>->Void> = new Event<Array<IAPPurchase>->Void>();
+
+	/**
+	 * Event dispatched when an error occurs during IAP processing.
+	 * @param errorMessage A string representing the error message.
+	 */
 	public static var onError(default, null):Event<String->Void> = new Event<String->Void>();
 
+	// Flag to indicate if the IAP system has been initialized.
 	@:noCompletion
 	private static var initialized:Bool = false;
 
+	/**
+	 * Initializes the IAP system with a public key for authentication.
+	 * @param publicKey The public key used for IAP authentication.
+	 */
 	public static function init(publicKey:String):Void
 	{
 		if (initialized)
@@ -39,6 +105,10 @@ class IAPAndroid
 		}
 	}
 
+	/**
+	 * Initiates a purchase for a product.
+	 * @param productDetails The IAPProductDetails object representing the product to be purchased.
+	 */
 	public static function purchase(productDetails:IAPProductDetails):Void
 	{
 		if (!initialized)
@@ -53,6 +123,10 @@ class IAPAndroid
 			purchaseJNI(productDetails.productId);
 	}
 
+	/**
+	 * Consumes a purchased product.
+	 * @param purchase The IAPPurchase object representing the purchase to be consumed.
+	 */
 	public static function consume(purchase:IAPPurchase):Void
 	{
 		if (!initialized)
@@ -67,6 +141,10 @@ class IAPAndroid
 			consumeJNI(purchase.stringifyedJson, purchase.signature);
 	}
 
+	/**
+	 * Acknowledges a purchased product.
+	 * @param purchase The IAPPurchase object representing the purchase to be acknowledged.
+	 */
 	public static function acknowledgePurchase(purchase:IAPPurchase):Void
 	{
 		if (!initialized)
@@ -82,6 +160,10 @@ class IAPAndroid
 			acknowledgePurchaseJNI(purchase.stringifyedJson, purchase.signature);
 	}
 
+	/**
+	 * Queries product details asynchronously.
+	 * @param ids An array of product IDs to query details for.
+	 */
 	public static function queryInAppProductDetailsAsync(ids:Array<String>):Void
 	{
 		if (!initialized)
@@ -97,6 +179,10 @@ class IAPAndroid
 			queryInAppProductDetailsJNI(ids);
 	}
 
+	/**
+	 * Queries subscription product details asynchronously.
+	 * @param ids An array of subscription product IDs to query details for.
+	 */
 	public static function querySubsProductDetailsAsync(ids:Array<String>):Void
 	{
 		if (!initialized)
@@ -112,6 +198,9 @@ class IAPAndroid
 			querySubsProductDetailsJNI(ids);
 	}
 
+	/**
+	 * Queries in-app purchases asynchronously.
+	 */
 	public static function queryInAppPurchasesAsync():Void
 	{
 		if (!initialized)
@@ -126,6 +215,9 @@ class IAPAndroid
 			queryInAppPurchasesJNI();
 	}
 
+	/**
+	 * Queries subscription purchases asynchronously.
+	 */
 	public static function querySubsPurchasesAsync():Void
 	{
 		if (!initialized)
@@ -233,7 +325,7 @@ private class CallBackHandler #if (lime >= "8.0.0") implements lime.system.JNI.J
 		for (productDetails in (haxe.Json.parse(result).purchases : Array<Dynamic>))
 			productsDetails.push(new IAPProductDetails(productDetails));
 
-		IAPAndroid.onQueryProductDetailsFinished.dispatch(result);
+		IAPAndroid.onQueryProductDetailsFinished.dispatch(productsDetails);
 	}
 
 	@:keep
