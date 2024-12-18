4cfe1df249951d0663ff3229ebbe831033d9aec9
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-18 11:03:27 +0200

    Update IAPIOS.hx

--- iap/ios/IAPIOS.hx
+++ iap/ios/IAPIOS.hx
@@ -20,6 +20,24 @@ class IAPIOS
 		initialized = true;
 	}
 
+	public static function fetchProducts(productIdentifiers:Array<String>):Void
+	{
+		if (!initialized)
+			return;
+
+		final rawProductsArray:cpp.RawPointer<cpp.ConstCharStar> = untyped __cpp__('new const char *[{0}]', productIdentifiers.length);
+
+		for (i in 0...productIdentifiers.length)
+			rawProductsArray[i] = cpp.ConstCharStar.fromString(productIdentifiers[i]);
+
+		fetchProductsIAP(rawProductsArray, productIdentifiers.length);
+
+		untyped __cpp__('delete[] {0}', rawProductsArray);
+	}
+
 	@:native('initIAP')
 	extern public static function initIAP():Void;
+
+	@:native('fetchProductsIAP')
+	extern public static function fetchProductsIAP(productIdentifiers:cpp.RawPointer<cpp.ConstCharStar>, count:cpp.SizeT):Void;
 }
