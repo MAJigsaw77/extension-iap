0a1a2ad40267baae84ad00d5b9f123b404d6891a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-19 09:12:08 +0200

    Update IAPIOS.hx

--- iap/ios/IAPIOS.hx
+++ iap/ios/IAPIOS.hx
@@ -1,6 +1,6 @@
 package iap.ios;
 
-@:buildXml('<include name="${haxelib:extension-iap}/project/iap-ios/Build.xml" />')
+@:buildXml('<include name="${haxelib:extension - iap}/project/iap-ios/Build.xml" />')
 @:headerInclude('IAP.hpp')
 class IAPIOS
 {
@@ -33,8 +33,31 @@ class IAPIOS
 	}
 
 	@:native('initIAP')
-	extern public static function initIAP():Void;
+	extern public static function initIAP(callbacks:BillingCallbacks):Void;
 
 	@:native('fetchProductsIAP')
 	extern public static function fetchProductsIAP(productIdentifiers:cpp.RawPointer<cpp.ConstCharStar>, count:cpp.SizeT):Void;
+
+	@:native('purchaseProductIAP')
+	extern public static function purchaseProductIAP(productId:cpp.ConstCharStar):Void;
+
+	@:native('restorePurchasesIAP')
+	extern public static function restorePurchasesIAP():Void;
+}
+
+@:dox(hide)
+@:buildXml('<include name="${haxelib:extension - iap}/project/iap-ios/Build.xml" />')
+@:include('IAP.hpp')
+@:unreflective
+@:structAccess
+@:native('BillingCallbacks')
+extern class BillingCallbacks
+{
+	function new():Void;
+
+	var onBillingClientSetup:cpp.Callable<(success:Bool) -> Void>;
+	var onBillingClientDebugLog:cpp.Callable<(message:cpp.ConstCharStar) -> Void>;
+	var onQueryProductDetails:cpp.Callable<(productDetails:cpp.RawPointer<cpp.ConstCharStar>, count:Int) -> Void>;
+	var onPurchaseCompleted:cpp.Callable<(productId:cpp.ConstCharStar) -> Void>;
+	var onRestoreCompleted:cpp.Callable<(productIds:cpp.RawPointer<cpp.ConstCharStar>, count:Int) -> Void>;
 }
