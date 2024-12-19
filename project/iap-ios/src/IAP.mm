2dcabae4ecbcde28ed58fe9435d83f556f74bbda
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-19 09:04:14 +0200

    Update IAP.mm

--- project/iap-ios/src/IAP.mm
+++ project/iap-ios/src/IAP.mm
@@ -3,13 +3,168 @@
 #import <Foundation/Foundation.h>
 #import <StoreKit/StoreKit.h>
 
-@interface IAP : NSObject <SKProductsRequestDelegate, SKPaymentTransactionObserver>
+@protocol BillingUpdatesListener <NSObject>
+- (void)onBillingClientSetup:(BOOL)success;
+- (void)onBillingClientDebugLog:(NSString *)message;
+- (void)onQueryProductDetails:(NSArray<SKProduct *> *)productDetails;
+- (void)onPurchaseCompleted:(SKPaymentTransaction *)transaction;
+- (void)onRestoreCompleted:(NSArray<SKPaymentTransaction *> *)transactions;
+@end
 
-@property (nonatomic, strong) NSArray<SKProduct *> *availableProducts;
+@interface BillingManager : NSObject <SKProductsRequestDelegate, SKPaymentTransactionObserver>
 
-+ (instancetype)sharedInstance;
-- (void)initIAP;
+@property (nonatomic, weak) id<BillingUpdatesListener> billingUpdatesListener;
+@property (nonatomic, strong) NSMutableArray<SKProduct *> *availableProducts;
 
+- (instancetype)initWithUpdatesListener:(id<BillingUpdatesListener>)listener;
+- (void)startConnection;
+- (void)queryProductDetails:(NSArray<NSString *> *)productIdentifiers;
+- (void)initiatePurchaseFlow:(NSString *)productIdentifier;
+- (void)restorePurchases;
+- (void)destroy;
+
+@end
+
+@implementation BillingManager
+
+- (instancetype)initWithUpdatesListener:(id<BillingUpdatesListener>)listener
+{
+	self = [super init];
+
+	if (self)
+	{
+		self.billingUpdatesListener = listener;
+		self.availableProducts = [NSMutableArray array];
+	}
+
+	return self;
+}
+
+- (void)startConnection
+{
+	[[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+
+	[self.billingUpdatesListener onBillingClientSetup:YES];
+
+	[self.billingUpdatesListener onBillingClientDebugLog:@"Billing connection started."];
+}
+
+- (void)queryProductDetails:(NSArray<NSString *> *)productIdentifiers
+{
+	if (!productIdentifiers || productIdentifiers.count == 0)
+	{
+		[self.billingUpdatesListener onBillingClientDebugLog:@"No product identifiers provided."];
+
+		return;
+	}
+
+	SKProductsRequest *productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:[NSSet setWithArray:productIdentifiers]];
+	productsRequest.delegate = self;
+	[productsRequest start];
+}
+
+- (void)initiatePurchaseFlow:(NSString *)productIdentifier
+{
+	SKProduct *product = [self findProductByIdentifier:productIdentifier];
+
+	if (!product)
+	{
+		[self.billingUpdatesListener onBillingClientDebugLog:[NSString stringWithFormat:@"Product not found: %@", productIdentifier]];
+		return;
+	}
+
+	SKPayment *payment = [SKPayment paymentWithProduct:product];
+
+	[[SKPaymentQueue defaultQueue] addPayment:payment];
+}
+
+- (void)restorePurchases
+{
+	[[SKPaymentQueue defaultQueue] restoreCompletedTransactions];
+}
+
+- (void)destroy
+{
+	[[SKPaymentQueue defaultQueue] removeTransactionObserver:self];
+
+	[self.billingUpdatesListener onBillingClientDebugLog:@"Billing connection destroyed."];
+}
+
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response
+{
+	if (response.products.count > 0)
+	{
+		[self.availableProducts removeAllObjects];
+		[self.availableProducts addObjectsFromArray:response.products];
+		[self.billingUpdatesListener onQueryProductDetails:response.products];
+	}
+	else
+	{
+		[self.billingUpdatesListener onBillingClientDebugLog:@"No products found."];
+	}
+}
+
+- (void)request:(SKRequest *)request didFailWithError:(NSError *)error
+{
+	[self.billingUpdatesListener onBillingClientDebugLog:[NSString stringWithFormat:@"Failed to fetch products: %@", error.localizedDescription]];
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray<SKPaymentTransaction *> *)transactions
+{
+	for (SKPaymentTransaction *transaction in transactions)
+	{
+		switch (transaction.transactionState)
+	{
+			case SKPaymentTransactionStatePurchased:
+				[self.billingUpdatesListener onPurchaseCompleted:transaction];
+
+				[[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+
+				break;
+			case SKPaymentTransactionStateRestored:
+				[self.billingUpdatesListener onRestoreCompleted:queue.transactions];
+
+				[[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+
+				break;
+			case SKPaymentTransactionStateFailed:
+				[self.billingUpdatesListener onBillingClientDebugLog:[NSString stringWithFormat:@"Purchase failed: %@", transaction.error.localizedDescription]];
+
+				[[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+- (void)paymentQueueRestoreCompletedTransactionsFinished:(SKPaymentQueue *)queue
+{
+	[self.billingUpdatesListener onRestoreCompleted:queue.transactions];
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue restoreCompletedTransactionsFailedWithError:(NSError *)error
+{
+	[self.billingUpdatesListener onBillingClientDebugLog:[NSString stringWithFormat:@"Failed to restore purchases: %@", error.localizedDescription]];
+}
+
+- (SKProduct *)findProductByIdentifier:(NSString *)identifier
+{
+	for (SKProduct *product in self.availableProducts)
+	{
+		if ([product.productIdentifier isEqualToString:identifier])
+			return product;
+	}
+
+	return nil;
+}
+
+@end
+
+@interface IAP() <BillingUpdatesListener>
+@property (nonatomic, strong) BillingManager *billingManager;
+@property (nonatomic, assign) BillingCallbacks callbacks;
 @end
 
 @implementation IAP
@@ -21,94 +176,95 @@
 	static dispatch_once_t onceToken;
 
 	dispatch_once(&onceToken, ^{
-		sharedInstance = [[self alloc] init];
+		sharedInstance = [[IAP alloc] init];
 	});
 
 	return sharedInstance;
 }
 
-- (void)initIAP
+- (instancetype)init
 {
-	NSLog(@"Initializing In-App Purchases...");
+	self = [super init];
 
-	[[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+	if (self)
+		_billingManager = [[BillingManager alloc] initWithUpdatesListener:self];
+
+	return self;
 }
 
-- (void)fetchProducts:(NSArray<NSString *> *)productIdentifiers
+- (void)setCallbacks:(BillingCallbacks)callbacks
 {
-	NSLog(@"Fetching products...");
-
-	SKProductsRequest *productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:[NSSet setWithArray:productIdentifiers]];
-	productsRequest.delegate = self;
-	[productsRequest start];
+	_callbacks = callbacks;
 }
 
-- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response
+- (void)onBillingClientSetup:(BOOL)success
 {
-	NSLog(@"Received product response");
-
-	self.availableProducts = response.products;
-
-	for (SKProduct *product in self.availableProducts)
-		NSLog(@"Product found: %@ - %@", product.localizedTitle, product.price);
-
-	if (response.invalidProductIdentifiers.count > 0)
-		NSLog(@"Invalid product identifiers: %@", response.invalidProductIdentifiers);
+	if (self.callbacks.onBillingClientSetup)
+		self.callbacks.onBillingClientSetup(success);
 }
 
-- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray<SKPaymentTransaction *> *)transactions
+- (void)onBillingClientDebugLog:(NSString *)message
 {
-	for (SKPaymentTransaction *transaction in transactions)
+	if (self.callbacks.onBillingClientDebugLog)
+		self.callbacks.onBillingClientDebugLog([message UTF8String]);
+}
+
+- (void)onQueryProductDetails:(NSArray<SKProduct *> *)productDetails
+{
+	if (self.callbacks.onQueryProductDetails)
 	{
-		switch (transaction.transactionState)
-		{
-			case SKPaymentTransactionStatePurchased:
-				NSLog(@"Transaction successful: %@", transaction.payment.productIdentifier);
-				[[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-				break;
-			case SKPaymentTransactionStateFailed:
-				NSLog(@"Transaction failed: %@", transaction.error.localizedDescription);
-				[[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-				break;
-			case SKPaymentTransactionStateRestored:
-				NSLog(@"Transaction restored: %@", transaction.payment.productIdentifier);
-				[[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-				break;
-			default:
-				break;
-		}
+		NSMutableArray<const char*> *productDetailsArray = [NSMutableArray arrayWithCapacity:productDetails.count];
+  
+		for (SKProduct *product in productDetails)
+			[productDetailsArray addObject:[product.productIdentifier UTF8String]];
+
+		self.callbacks.onQueryProductDetails(productDetailsArray.mutableBytes, productDetails.count);
 	}
 }
 
-- (void)dealloc
+- (void)onPurchaseCompleted:(SKPaymentTransaction *)transaction
 {
-	[[SKPaymentQueue defaultQueue] removeTransactionObserver:self];
+	if (self.callbacks.onPurchaseCompleted)
+		self.callbacks.onPurchaseCompleted([transaction.payment.productIdentifier UTF8String]);
 }
 
+- (void)onRestoreCompleted:(NSArray<SKPaymentTransaction *> *)transactions
+{
+	if (self.callbacks.onRestoreCompleted)
+	{
+		NSMutableArray<const char*> *restoredProducts = [NSMutableArray arrayWithCapacity:transactions.count];
+
+		for (SKPaymentTransaction *transaction in transactions)
+			[restoredProducts addObject:[transaction.payment.productIdentifier UTF8String]];
+
+		self.callbacks.onRestoreCompleted(restoredProducts.mutableBytes, transactions.count);
+	}
+}
 @end
 
-void initIAP()
+void initIAP(BillingCallbacks callbacks)
 {
-	[[IAP sharedInstance] initIAP];
+	IAP *iap = [IAP sharedInstance];
+	[iap setCallbacks:callbacks];
+	[[iap billingManager] startConnection];
 }
 
 void fetchProductsIAP(const char** productIdentifiers, size_t count)
 {
-	if (count == 0 || productIdentifiers == nullptr)
-	{
-		NSLog(@"No product identifiers provided.");
-		return;
-	}
-
 	NSMutableArray<NSString *> *objcProductIdentifiers = [NSMutableArray array];
 
 	for (size_t i = 0; i < count; ++i)
-	{
-		if (productIdentifiers[i] != nullptr)
-			[objcProductIdentifiers addObject:[NSString stringWithUTF8String:productIdentifiers[i]]];
-	}
-
-	NSLog(@"Fetching products through fetchProductsIAP...");
+		[objcProductIdentifiers addObject:[NSString stringWithUTF8String:productIdentifiers[i]]];
 
 	[[IAP sharedInstance] fetchProducts:objcProductIdentifiers];
+}
+
+void purchaseProductIAP(const char* productId)
+{
+	[[IAP sharedInstance] purchaseProduct:[NSString stringWithUTF8String:productId]];
+}
+
+void restorePurchasesIAP()
+{
+	[[IAP sharedInstance] restorePurchases];
 }
