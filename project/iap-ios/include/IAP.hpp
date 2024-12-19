1d7ad3ec705841f28c41ae96639c68d15067e532
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-19 08:58:35 +0200

    Update IAP.hpp

--- project/iap-ios/include/IAP.hpp
+++ project/iap-ios/include/IAP.hpp
@@ -1,4 +1,14 @@
 #pragma once
 
-void initIAP();
-void fetchProductsIAP(const char** productIdentifiers, size_t count);
+typedef struct {
+    void (*onBillingClientSetup)(bool success);
+    void (*onBillingClientDebugLog)(const char* message);
+    void (*onQueryProductDetails)(const char* productDetails[], size_t count);
+    void (*onPurchaseCompleted)(const char* productId);
+    void (*onRestoreCompleted)(const char* productIds[], size_t count);
+} BillingCallbacks;
+
+void initIAP(BillingCallbacks callbacks)
+void fetchProductsIAP(const char** productIdentifiers, size_t count)
+void purchaseProductIAP(const char* productId)
+void restorePurchasesIAP()
