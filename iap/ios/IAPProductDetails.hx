516ba2084fca12ff90371689b36480c5ee49663d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-19 15:15:35 +0200

    Create IAPProductDetails.hx

---
+++ iap/ios/IAPProductDetails.hx
@@ -0,0 +1,19 @@
+package iap.ios;
+
+class IAPProductDetails
+{
+	public final productIdentifier:String;
+	public final localizedTitle:String;
+	public final localizedDescription:String;
+	public final price:Float;
+	public final priceLocale:String;
+
+	public function new(json:Dynamic):Void
+	{
+		productIdentifier = json.productIdentifier;
+		localizedTitle = json.localizedTitle;
+		localizedDescription = json.localizedDescription;
+		price = Std.parseFloat(json.price);
+		priceLocale = json.priceLocale;
+	}
+}
