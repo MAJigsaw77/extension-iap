846aa8b42fc3cfb22f25f7a03b7df8339a3c32c9
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-19 20:30:40 +0200

    Update IAPPurchase.hx

--- iap/ios/IAPPurchase.hx
+++ iap/ios/IAPPurchase.hx
@@ -1,7 +1,5 @@
 package iap.ios;
 
-import iap.ios.IAPDownload;
-
 class IAPPurchase
 {
 	public final transactionId:String;
@@ -9,19 +7,11 @@ class IAPPurchase
 	public final transactionDate:Int;
 	public final transactionState:Int;
 
-	public var downloads(default, null):Array<IAPDownload> = [];
-
 	public function new(json:Dynamic):Void
 	{
 		transactionId = json.transactionId;
 		productIdentifier = json.productIdentifier;
 		transactionDate = json.transactionDate;
 		transactionState = json.transactionState;
-
-		if (json.downloads != null)
-		{
-			for (downloadJson in (json.downloads : Array<Dynamic>))
-				downloads.push(new IAPDownload(downloadJson));
-		}
 	}
 }
